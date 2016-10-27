c7bfdcc4f5934b3e23f317595c816a26b80d6774
Ajkea <hendrik.vandijck@student.kdg.be>
2016-10-27 13:47:08 +0200

    update oefeningen 27/10

---
+++ opdracht-sessions/oplossing-sessions-deel1.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Opdracht sessions: deel 1</title>
+	<style type="text/css">body{
+		padding:1em;
+		font-family: arial;}
+	</style>
+</head>
+<body>
+<form action="oplossing-sessions-deel1.php" method="POST">
+	<label for="email">email:</label><input type="email" id="email" name="email"><br>
+	<label for="nickname">nickname:</label><input type="text" id="nickname" name="nickname">
+</form>
+</body>
+</html>
